45f64383e37823e1b38203be999c944b361bb7d0
arsianindita <puspita.parahita@gmail.com>
2014-09-28 22:42:47 +0700

    Update DreamyForm.java
    - DreamyForm.java updated
    - Add dreamy_form_row.xml

--- Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
@@ -1,10 +1,40 @@
 package com.breworks.dreamy;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by arsianindita on 28-Sep-14.
  */
-public class DreamyForm extends Activity{
+public class DreamyForm extends Activity implements View.OnClickListener {
+
+    LinearLayout container;
+    Button addMilestone;
+    EditText milestoneInput;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dreamy_form);
+        addMilestone = (Button) findViewById(R.id.addMilestone);
+        milestoneInput = (EditText) findViewById(R.id.milestoneInput);
+        container = (LinearLayout) findViewById(R.id.container);
+
+        addMilestone.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+
+
+
+
+
+
+        
+    }
     
 }
