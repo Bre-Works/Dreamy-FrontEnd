0601b04dfc31db0fc097bfc94c3fdfdf19dbe8e2
aidifauzan <aidi.vh@gmail.com>
2014-09-28 17:51:07 +0700

    set theme for action bar instead of removing it

--- Dreamy/app/src/main/java/com/breworks/dreamy/Main.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/Main.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 /**
  * Created by aidifauzan on 24-Sep-14.
  */
@@ -14,8 +13,6 @@ public class Main extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
-        getActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
