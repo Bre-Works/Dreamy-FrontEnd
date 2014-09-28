fa9fdc9e3367ffb2a9d0250425fbef0dcd761010
arsianindita <puspita.parahita@gmail.com>
2014-09-29 02:44:55 +0700

    Dreamy Form
    - Update DreamyForm.java
    - Update dreamy_form.xml
    - Update dreamy_row.xml
    - Add image button; remove and add button

--- Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
@@ -1,11 +1,14 @@
 package com.breworks.dreamy;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by arsianindita on 28-Sep-14.
@@ -13,14 +16,15 @@ import android.widget.LinearLayout;
 public class DreamyForm extends Activity implements View.OnClickListener {
 
     LinearLayout container;
-    Button addMilestone;
+    ImageButton addMilestone;
     EditText milestoneInput;
+    ImageButton removeMilestone;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dreamy_form);
-        addMilestone = (Button) findViewById(R.id.addMilestone);
+        addMilestone = (ImageButton) findViewById(R.id.addMilestone);
         milestoneInput = (EditText) findViewById(R.id.milestoneInput);
         container = (LinearLayout) findViewById(R.id.container);
 
@@ -29,12 +33,26 @@ public class DreamyForm extends Activity implements View.OnClickListener {
 
     public void onClick(View v){
 
+        LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        final View addView = inflater.inflate(R.layout.dreamy_form_row, null);
 
+        removeMilestone = (ImageButton)addView.findViewById(R.id.delMilestone);
 
+        TextView milestoneOut = (TextView)addView.findViewById(R.id.milestoneOut);
 
+        milestoneOut.setText(milestoneInput.getText().toString());
 
+        removeMilestone.setOnClickListener(new View.OnClickListener() {
 
-        
+            @Override
+            public void onClick(View v1) {
+                ((LinearLayout) addView.getParent()).removeView(addView);
+            }
+        });
+
+        container.addView(addView);
+        milestoneInput.setText("");
     }
     
 }
