f01fee7051fd42770904e70932268be228f54463
aidifauzan <aidi.vh@gmail.com>
2014-09-28 15:27:20 +0700

    remove action bar

--- Dreamy/app/src/main/java/com/breworks/dreamy/Main.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/Main.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.Window;
+/**
+ * Created by aidifauzan on 24-Sep-14.
+ */
 
 public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
