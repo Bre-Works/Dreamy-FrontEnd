5c0b0a5d7ffb442255bc594fb24dc55671c00444
arsianindita <puspita.parahita@gmail.com>
2014-10-02 12:01:43 +0700

    Add milestone using enter key

--- Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/DreamyForm.java
@@ -3,6 +3,7 @@ package com.breworks.dreamy;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -14,7 +15,7 @@ import android.widget.TextView;
 /**
  * Created by arsianindita on 28-Sep-14.
  */
-public class DreamyForm extends Activity implements View.OnClickListener {
+public class DreamyForm extends Activity{
 
     LinearLayout container;
     ImageButton addMilestone;
@@ -25,36 +26,49 @@ public class DreamyForm extends Activity implements View.OnClickListener {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dreamy_form);
-        addMilestone = (ImageButton) findViewById(R.id.addMilestone);
+        //addMilestone = (ImageButton) findViewById(R.id.addMilestone);
         milestoneInput = (EditText) findViewById(R.id.milestoneInput);
         container = (LinearLayout) findViewById(R.id.container);
 
-        addMilestone.setOnClickListener(this);
+        //addMilestone.setOnClickListener(this);
+
+        milestoneInput.setOnKeyListener(new View.OnKeyListener() {
+
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                    if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                            LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService
+                                    (Context.LAYOUT_INFLATER_SERVICE);
+
+                            final View addView = inflater.inflate(R.layout.dreamy_form_row, null);
+
+                            removeMilestone = (ImageButton) addView.findViewById(R.id.delMilestone);
+
+                            EditText milestoneOut = (EditText) addView.findViewById(R.id.milestoneOut);
+
+                            milestoneOut.setText(milestoneInput.getText().toString());
+
+                            removeMilestone.setOnClickListener(new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View v1) {
+                                    ((LinearLayout) addView.getParent()).removeView(addView);
+                                }
+                            });
+
+                            container.addView(addView);
+                            milestoneInput.setText("");
+
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            });
     }
 
-    public void onClick(View v){
 
-        LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService
-                (Context.LAYOUT_INFLATER_SERVICE);
 
-        final View addView = inflater.inflate(R.layout.dreamy_form_row, null);
-
-        removeMilestone = (ImageButton)addView.findViewById(R.id.delMilestone);
-
-        EditText milestoneOut = (EditText)addView.findViewById(R.id.milestoneOut);
-
-        milestoneOut.setText(milestoneInput.getText().toString());
-
-        removeMilestone.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v1) {
-                ((LinearLayout) addView.getParent()).removeView(addView);
-            }
-        });
-
-        container.addView(addView);
-        milestoneInput.setText("");
-    }
-    
 }
+
