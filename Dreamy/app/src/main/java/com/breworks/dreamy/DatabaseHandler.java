b046db20ffe55b5a433e3f564d56f7d18fdc87ba
Risdani Agung <eater.luck@gmail.com>
2014-10-04 17:47:40 +0700

    added account integration (not finished)

---
+++ Dreamy/app/src/main/java/com/breworks/dreamy/DatabaseHandler.java
@@ -0,0 +1,67 @@
+package com.breworks.dreamy;
+
+import android.accounts.Account;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Luck Eater on 10/4/2014.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database Name
+    private static final String DATABASE_NAME = "dreamyDB";
+
+    //account table name
+    private static final String TABLE_ACCOUNTS = "Dreamy_account";
+
+    //account table column names
+    private static final String KEY_ACCOUNT = "accountName";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASS = "password";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    //Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_ACCOUNTS_TABLE = "CREATE TABLE " + TABLE_ACCOUNTS + "(" + KEY_ACCOUNT +
+                " TEXT PRIMARY KEY NOT NULL UNIQUE,"+ KEY_EMAIL + "TEST NOT NULL UNIQUE," +
+                KEY_PASS + "TEXT NOT NULL" +")";
+        db.execSQL(CREATE_ACCOUNTS_TABLE);
+    }
+    //Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        //drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS"+ TABLE_ACCOUNTS);
+
+        //Create tables again
+        onCreate(db);
+    }
+
+    //adding new account
+    public void addAccount(Account account){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ACCOUNT,account.getAccount);
+    }
+
+    //updating single account
+    public int updateAccount(Account account){}
+    //deleting single account
+    public void deleteAccount(Account account){}
+
+
+
+    }
+
---
+++ Dreamy/app/src/main/java/com/breworks/dreamy/dreamyAccount.java
@@ -0,0 +1,53 @@
+package com.breworks.dreamy;
+
+/**
+ * Created by Luck Eater on 10/4/2014.
+ */
+public class dreamyAccount {
+    //private variable
+    String _accountName;
+    String _email;
+    String _password;
+
+    //empty constructor
+    public dreamyAccount(){
+
+    }
+
+    //constructor
+    public dreamyAccount(String accountName, String email, String password){
+        this._accountName = accountName;
+        this._email = email;
+        this._password = password;
+    }
+
+    //getting accountName
+    public String getAccount(){
+        return this._accountName;
+    }
+
+    //setting accountName
+    public void setAccount(String accountName){
+        this._accountName = accountName;
+    }
+
+    //getting email
+    public String getEmail(){
+        return this._email;
+    }
+
+    //setting email
+    public void setEmail(String email){
+        this._email = email;
+    }
+
+    //getting password
+    public String getPassword(){
+        return this._password;
+    }
+
+    //setting password
+    public void setPassword(String password){
+        this._password = password;
+    }
+}
