f670c813524bc524c3f40e1a433943abd4cb0b0c
arsianindita <puspita.parahita@gmail.com>
2014-10-05 22:24:13 +0700

    Password confirmation mismatch alert
    - update signUp.java
    
    alert will popped up if the password and the confirmation did not match

--- Dreamy/app/src/main/java/com/breworks/dreamy/signUp.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/signUp.java
@@ -11,11 +11,35 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class signUp extends Activity {
-    @Override
+public class signUp extends Activity{
+    Button createAccount;
+    EditText usernameInput, emailInput, passwordInput, passwordConfInput;
+    String username, email, password, passwordConf;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        usernameInput = (EditText) findViewById(R.id.username);
+        emailInput = (EditText) findViewById(R.id.email);
+        passwordInput = (EditText) findViewById(R.id.password);
+        passwordConfInput = (EditText) findViewById(R.id.passwordConf);
+        createAccount = (Button) findViewById(R.id.createAccount);
+    }
+
+
+    public void createAccount(View v){
+        username = usernameInput.toString();
+        email = emailInput.toString();
+        password = passwordInput.toString();
+        passwordConf = passwordConfInput.toString();
+
+        if(!password.equals(passwordConf)){
+            Toast.makeText(getApplicationContext(), "Password and password confirmation did not match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
