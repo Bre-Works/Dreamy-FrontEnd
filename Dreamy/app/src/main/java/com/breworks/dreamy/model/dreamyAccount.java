1b240a506f479cb5c1de30029599d5b829b81ff2
arsianindita <puspita.parahita@gmail.com>
2014-10-09 12:44:53 +0700

    account db

--- Dreamy/app/src/main/java/com/breworks/dreamy/model/dreamyAccount.java
+++ Dreamy/app/src/main/java/com/breworks/dreamy/model/dreamyAccount.java
@@ -22,6 +22,11 @@ public class dreamyAccount extends SugarRecord<dreamyAccount> {
             this.username = username;
         }
 
+        public void createAccount(String email, String username, String password){
+            dreamyAccount account = new dreamyAccount(email, username, password);
+            account.save();
+        }
+
 
 
 
